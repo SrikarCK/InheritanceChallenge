f0a07bb14cee1f02cc7abcf769d1287b9aaa9180
srikar <srikarkc@gmail.com>
2019-01-10 11:41:22 -0600

    Concept of Inheritance.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Bmw x3 = new Bmw(36);
+        x3.steer(45);
+        x3.accelerate(30);
+        x3.accelerate(20);
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,45 @@
+public class Vehicle {
+    private String name;
+    private String size;
+
+    private int currentvelocity;
+    private int currentDriection;
+
+    public Vehicle(String name, String size){
+        this.name = name;
+        this.size = size;
+
+        this.currentvelocity = 0;
+        this.currentDriection = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public int getCurrentvelocity() {
+        return currentvelocity;
+    }
+
+    public int getCurrentDriection() {
+        return currentDriection;
+    }
+
+    public void steer(int direction){
+        this.currentDriection += direction;
+        System.out.println("Vehicle.steer(): Steering at "+currentDriection+" degrees.");
+    }
+    public void move(int velocity, int direction){
+        currentvelocity = velocity;
+        currentDriection = direction;
+        System.out.println("Vehicle.move(): Moving at "+currentvelocity+" in direction "+currentDriection);
+    }
+
+    public void stop(){
+        this.currentvelocity = 0;
+    }
+}
